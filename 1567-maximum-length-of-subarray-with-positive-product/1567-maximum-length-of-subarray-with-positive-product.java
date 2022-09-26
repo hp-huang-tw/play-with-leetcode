1d56189e9fce422d2cba84ad75042aa5acb692ac
hp.huang <stitchmos@gmail.com>
2022-09-26 21:51:44 +0800

    Time: 5 ms (70.70%), Space: 76.8 MB (80.81%) - LeetHub

---
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int getMaxLen(int[] nums) {
+        int res=0, countPositive=0, countNegative=0;
+        
+        for (int i=0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                countPositive = 0;
+                countNegative = 0;
+            } else if (nums[i] > 0) {
+                // 1,  -2
+                // 1,1 0,2
+                countPositive++;
+                countNegative = (countNegative == 0) ? 0 : (countNegative + 1);
+            } else {
+                int tmpPositive = countPositive;
+                countPositive = (countNegative == 0) ? 0 : (countNegative + 1);
+                countNegative = tmpPositive+1;
+               
+            }
+            
+            res=Math.max(res, countPositive);
+        }
+        
+        return res;
+    }
+}
