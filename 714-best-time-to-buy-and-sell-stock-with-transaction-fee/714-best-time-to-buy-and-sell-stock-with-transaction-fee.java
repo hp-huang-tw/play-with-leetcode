2df82aa0143c73a60e69bc62f93225cf0998e2d2
hp.huang <stitchmos@gmail.com>
2022-09-29 11:23:24 +0800

    Time: 3 ms (100.00%), Space: 50.8 MB (92.35%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -7,6 +7,15 @@ class Solution {
     private int[][] mem;
     
     public int maxProfit(int[] prices, int fee) {
+        int cash = 0, hold = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            cash = Math.max(cash, hold + prices[i] - fee);
+            hold = Math.max(hold, cash - prices[i]);
+        }
+        return cash;
+    }
+    
+    public int maxProfit_withMem(int[] prices, int fee) {
         
         this.prices = prices;
         this.fee = fee;
