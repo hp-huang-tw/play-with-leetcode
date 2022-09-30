42008e9f9669b3e2f562f884b54b0cae154eef8a
hp.huang <stitchmos@gmail.com>
2022-09-30 11:50:54 +0800

    Time: 6 ms (80.67%), Space: 43 MB (71.49%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return topDown(s, wordDict);
+        return bottomUp(s, wordDict);
     }
     
     private boolean topDown(String s, List<String> wordDict) {
         // s = "leetcode", wordDict = ["leet","code"]
-        //     TFFFTFFFT  
+        // dp  TFFFTFFFT  
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
         for (int i = 1; i <= s.length(); i++) {
@@ -20,5 +20,27 @@ class Solution {
         }
         
         return dp[dp.length-1];
+    }
+    
+    private boolean bottomUp(String s, List<String> wordDict) {
+        // s = "leetcode", wordDict = ["leet","code"]
+        // dp          T
+        // i =        7
+        
+        int strLength = s.length();
+        boolean[] dp = new boolean[strLength + 1];
+        dp[strLength] = true;
+        
+        for (int i = strLength-1; i > -1;  i--) {
+            for (String word : wordDict) {
+                int wordLength = word.length();
+                if (i+wordLength <= strLength && s.substring(i, i+wordLength).equals(word)) {
+                    dp[i] = dp[i+wordLength];
+                }
+                if (dp[i]) break;
+            }
+        }
+        
+        return dp[0];
     }
 }
