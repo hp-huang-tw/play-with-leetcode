c80ee768569c713bf8e4788e451d40bad0284eaf
hp.huang <stitchmos@gmail.com>
2022-10-03 21:15:44 +0800

    Time: 3 ms (99.14%), Space: 42.2 MB (95.94%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,6 +1,36 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        return buildin(s);
+        return twoPointer(s);
+    }
+    
+    private boolean twoPointer(String s) {
+        int left=0, right=s.length()-1;
+        
+        while (left < right) {            
+            while (left < right && !isAlphanumeric(s.charAt(left))) {
+                left++;
+            }
+            
+            while (left < right && !isAlphanumeric(s.charAt(right))) {
+                right--;
+            }
+            
+            if (Character.toLowerCase(s.charAt(left)) != 
+                Character.toLowerCase(s.charAt(right))) 
+                return false;
+            
+            left++;
+            right--;
+            
+        }
+        
+        return true;
+    }
+    
+    private boolean isAlphanumeric(Character c) {
+        return 'A' <= c && c <= 'Z' 
+            || 'a' <= c && c <= 'z'
+            || '0' <= c && c <= '9';
     }
     
     private boolean buildin(String s) {
