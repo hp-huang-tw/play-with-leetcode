191adf9fdc69b2ffee5b622ce300bb6ddbbd3944
hp.huang <stitchmos@gmail.com>
2022-10-05 22:43:15 +0800

    Time: 41 ms (5.04%), Space: 50.8 MB (91.37%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.java
@@ -0,0 +1,59 @@
+class Solution {
+    
+    private char[][] grid;
+    
+    private int numOfIslands=0;
+    
+    private HashSet<Pair> visited = new HashSet<Pair>();
+    
+    private int rowLen = 0;
+    
+    private int colLen = 0;
+    
+    public int numIslands(char[][] grid) {
+        this.grid = grid;
+        
+        rowLen = grid.length;
+        colLen = grid[0].length;
+        
+        for (int row=0; row < rowLen; row++) {
+            for (int col=0; col < colLen; col++) {
+                if (grid[row][col] == '1' && !visited.contains(new Pair(row, col))) {
+                    bfs(row, col);
+                    numOfIslands++;
+                }
+            }
+        }
+        return numOfIslands;
+    }
+    
+    private void bfs(int row, int col) {
+        Pair<Integer, Integer> currentPosition = new Pair(row, col);
+        
+        Deque<Pair> q = new LinkedList<Pair>();
+        q.add(currentPosition);
+        visited.add(currentPosition);
+        
+        while (!q.isEmpty()) {
+            Pair<Integer, Integer> position = q.poll();
+            
+            List<Pair<Integer, Integer>> directions = 
+                List.of(new Pair(1, 0), new Pair(-1, 0), new Pair(0, 1), new Pair(0, -1));
+            
+            
+            for (Pair<Integer, Integer> direction : directions) {
+                int nextRow = position.getKey() + direction.getKey();
+                int nextCol = position.getValue() + direction.getValue();
+                Pair<Integer, Integer> nextPosition = new Pair(nextRow, nextCol);
+                
+                // check not out of bound, not visit yet.
+                if (-1 < nextRow && nextRow < rowLen &&  -1 < nextCol && nextCol < colLen 
+                   && !visited.contains(nextPosition) 
+                    && grid[nextRow][nextCol] == '1') {               
+                    q.add(nextPosition);
+                    visited.add(nextPosition);
+                }
+            }
+        }
+    }
+}
