c33029eafc245042bc68cac470facb96038f8768
hp.huang <stitchmos@gmail.com>
2022-10-05 17:09:13 +0800

    Time: 5 ms (42.26%), Space: 55.3 MB (54.92%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,7 +11,18 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        return hashSet(headA, headB);
+        return twoPointers(headA, headB);
+    }
+    
+    private ListNode twoPointers(ListNode headA, ListNode headB) {
+        ListNode l1 = headA, l2 = headB;
+        
+        while (l1 != l2) {
+            l1 = (l1 != null) ? l1.next : headB;
+            l2 = (l2 != null) ? l2.next : headA;
+        }
+        
+        return l1;
     }
     
     // time: O(n+m), space: O(n)
