75b95e979470512df64518f08c7bd63af5ba085a
hp.huang <stitchmos@gmail.com>
2022-10-06 13:31:52 +0800

    Time: 48 ms (5.09%), Space: 73 MB (8.35%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,6 +1,17 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        return hashSet(nums);
+        return sort(nums);
+    }
+    
+    // Time: O(nLog(n)), Space: O(1)
+    private boolean sort(int[] nums) {
+        Arrays.sort(nums);
+        
+        for (int i=0; i < nums.length-1; i++) {
+            if (nums[i] == nums[i+1]) return true;
+        }
+        
+        return false;
     }
     
     // Time: O(N), Space: O(N)
@@ -14,8 +25,7 @@ class Solution {
         return false;
     }
     
-    // Time: O(N^2) 
-    // Space: O(1)
+    // Time: O(N^2), Space: O(1)
     private boolean twoPointer(int[] nums) {
         for (int i = 0; i < nums.length -1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
