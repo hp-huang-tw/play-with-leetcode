6f58e5cb74a2ade57e4e9054b298a405410e2ab8
hp.huang <stitchmos@gmail.com>
2022-10-07 12:40:46 +0800

    Time: 24 ms (20.24%), Space: 55.3 MB (73.04%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,7 +1,7 @@
 class Solution {
     
     public int majorityElement(int[] nums) {
-       return sort(nums);
+       return hashMap(nums);
     }
     
     // sort: O(n), O(1)
@@ -10,5 +10,21 @@ class Solution {
         return nums[nums.length/2];
     }
     
-    
+    // HashMap: O(n), O(n)
+    public int hashMap(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        int res = 0;
+        int maxCount = 0;
+        for (int n: nums) {
+            int count = 1+map.getOrDefault(n, 0);
+            map.put(n, count);
+            if (count > maxCount) {
+                res = n;
+                maxCount = count;
+            }
+        }
+        
+        return res;
+    }
 }
