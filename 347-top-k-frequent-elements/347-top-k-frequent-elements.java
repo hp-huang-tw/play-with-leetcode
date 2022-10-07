0356eaed057ede502a1602779c32fc790c087cbd
hp.huang <stitchmos@gmail.com>
2022-10-07 12:01:13 +0800

    Time: 11 ms (93.43%), Space: 44.4 MB (99.68%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> numCountMap = new HashMap<Integer, Integer>();
+        
+        for (int num : nums) {
+            numCountMap.put(num, 1 + numCountMap.getOrDefault(num, 0));
+        }
+        
+        
+        HashMap<Integer, List<Integer>> freqMap = new HashMap<Integer, List<Integer>>();
+        for (int num: numCountMap.keySet()) {
+            Integer count = numCountMap.get(num);
+            List<Integer> numList = freqMap.getOrDefault(count, new ArrayList<Integer>());
+            numList.add(num);
+            freqMap.put(count, numList);
+        }
+        
+        int[] res = new int[k];
+        int j = 0;
+        for (int i = nums.length; i > -1; i--) {
+            if (freqMap.containsKey(i)) {
+                for (int num :freqMap.get(i)) {
+                    res[j] = num;
+                    j++;
+                    if (j==k) return res;
+                }   
+            }
+        }
+        
+        return res;
+    }
+}
