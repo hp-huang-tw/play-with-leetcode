2af035713dc4fffdbd2d287035a6640e7650b39a
hp.huang <stitchmos@gmail.com>
2022-10-08 18:53:43 +0800

    Time: 0 ms (100.00%), Space: 41.4 MB (64.14%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -16,14 +16,8 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p==null && q==null) return true;
-        if ((p != null && q == null) || (q != null && p == null))
-            return false;
+        if ((p != null && q == null) || (q != null && p == null)) return false;
         if (p.val != q.val) return false;
-        
-        if (!isSameTree(p.left, q.left) || !isSameTree(p.right, q.right)) {
-            return false;
-        }
-        
-        return true;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
