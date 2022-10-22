a6b87b3b3a96ad470b053ae1597a1fa0ce0ced4b
hp.huang <stitchmos@gmail.com>
2022-10-22 10:52:09 +0800

    Time: 18 ms (5.23%), Space: 40.7 MB (90.54%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,36 @@
+class Solution {
+    private int[] dp;
+    private String s;
+    public int numDecodings(String s) {
+        this.s = s;
+        int len = s.length();
+        dp = new int[len];
+        dp[len - 1] = 1;
+        
+        return dfs(0);
+        
+    }
+    
+    private int dfs(int i) {
+        if(i == s.length()) return 1;
+        
+        if (s.charAt(i) == '0') {
+            return 0;
+        }
+        
+        if (dp[i]!=0) {
+            return dp[i];
+        }
+        
+        int res = dfs(i+1);
+        
+        if (i + 1 < s.length() && 
+            (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i+1) >= '0' &&
+             s.charAt(i+1) <= '6')) {
+            res += dfs(i+2);
+        }
+        
+        dp[i] = res;
+        return res;
+    }
+}
