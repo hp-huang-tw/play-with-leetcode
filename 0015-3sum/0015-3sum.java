ac93a8f5eeb35bbdee7ff17748f654cb148c59d9
hp.huang <stitchmos@gmail.com>
2022-10-23 18:14:06 +0800

    Time: 33 ms (68.99%), Space: 58.9 MB (56.32%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    
+    // TC: O(n^2)
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0) {
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    res.add(List.of(nums[i], nums[l], nums[r]));
+                    l++;
+                    
+                    while (nums[l] == nums[l-1] && l < r) {
+                        l++;
+                    }
+                }
+            }
+        }
+        
+        return res;
+    }
+}
