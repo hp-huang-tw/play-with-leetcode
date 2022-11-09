6e8c83568ced418c10e2930586c6dddf37c9e155
hp.huang <stitchmos@gmail.com>
2022-11-09 17:50:35 +0800

    Time: 34 ms (51.65%), Space: 141.7 MB (48.61%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.java
+++ 2055-plates-between-candles/2055-plates-between-candles.java
@@ -3,28 +3,37 @@ class Solution {
     // TC: O(n+q), SC: O(n)
     public int[] platesBetweenCandles(String s, int[][] queries) {
         int strLen = s.length();
-        List<Integer> presum = new ArrayList<>();
+        List<Integer> presum = new ArrayList<>(); // ecah portion [2, 4, 7]
         int[] closestLeft = new int[strLen], closestRight = new int[strLen];
         
-        int sum = 0, index = -1;
+        //  *  *  |  *  * |  * * * |
+        //  1  2     3  4    5 6 7     // count
+        //.       2       4        7  // presum
+        // -1 -1  0  0. 0 1  1 1 1 2  // closetLeft
+        int count = 0, index = -1;
         for (int i = 0; i < strLen; i++) {
-            if (s.charAt(i) == '*') sum++;
-            else {
-                presum.add(sum);
+            if (s.charAt(i) == '*') {
+                count++;
+            } else {
+                presum.add(count);
                 index++;
             }
             closestLeft[i] = index;
         }
 
-        index = presum.size();;
-        for (int i = strLen - 1; i >= 0; i--) {
+        index = presum.size(); // 3
+        //  *  *  |  *  * |  * * * |
+        //  0  0  1. 1 1  2 2 2 2         // closetRight
+        for (int i = strLen - 1; i > -1; i--) {
             if (s.charAt(i) == '|') index--;
             closestRight[i] = index;
         }
 
         int[] res = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            int start = queries[i][0], end = queries[i][1];
+            int start = queries[i][0];
+            int end = queries[i][1];
+            
             int left = closestRight[start];
             int right = closestLeft[end];
             if (left < right) res[i] = presum.get(right) - presum.get(left);
@@ -32,6 +41,7 @@ class Solution {
         return res;
     }
     
+    // timeout
     private int[] preSum(String s, int[][] queries) {
         int strLen = s.length();
         
