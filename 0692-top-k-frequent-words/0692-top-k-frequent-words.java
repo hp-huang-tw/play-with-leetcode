b09d8d0df932117d6c5dcc0ad7a039c131516580
hp.huang <stitchmos@gmail.com>
2022-11-15 12:06:27 +0800

    Time: 14 ms (43.57%), Space: 45.6 MB (16.43%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,17 +1,22 @@
 class Solution {
     Comparator<String> comparator;
     public List<String> topKFrequent(String[] words, int k) {
-        return bruteForce(words, k);
+        return minHeap(words, k);
     }
     
     
     // minHeap
+    // TC: O(nlog(k)). O(n) + O(nlog(k)) + O(klog(k))
+    // SC: O(n). O(n) + O(k)
     private List<String> minHeap(String[] words, int k) {
+        // TC: O(n)
         HashMap<String, Integer> wordCountMap = buidWordCountMap(words, k);
         
+        // k elmenets.
+        // TC: O(nlog(k)). O(log(k)) for add/remove n times
         PriorityQueue<String> q = new PriorityQueue<>(
             (w1, w2) -> wordCountMap.get(w1).equals(wordCountMap.get(w2)) ?
-                         w1.compareTo(w2) : wordCountMap.get(w2) - wordCountMap.get(w1));
+                         w2.compareTo(w1) : wordCountMap.get(w1) - wordCountMap.get(w2));
         
         for (String word: wordCountMap.keySet()) {
             q.offer(word);
@@ -20,12 +25,13 @@ class Solution {
             }
         }
         
+        // TC: O(klog(k))
         List<String> res = new ArrayList<>();
-        
         while (!q.isEmpty()) {
             res.add(q.poll());
         }
         
+        Collections.reverse(res);
         return res;
     }
     
