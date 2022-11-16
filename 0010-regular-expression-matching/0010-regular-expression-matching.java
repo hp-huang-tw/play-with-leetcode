5af0a486a25fab6fddf6718c7cb6b77489c4e60c
hp.huang <stitchmos@gmail.com>
2022-11-16 23:38:09 +0800

    Time: 58 ms (35.41%), Space: 43 MB (37.29%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -8,22 +8,16 @@ class Solution {
     
     int pLen;
     
-    Boolean[][] cached;
-    
     // TC: O(n * m)
     public boolean isMatch(String s, String p) {
         this.s = s;
         this.p = p;
         sLen = s.length();
         pLen = p.length();
-        cached = new Boolean[sLen + 1][pLen + 1];
         return dpTopDown(0, 0);
     }
     
-    // with memoization
     private boolean dpTopDown(int i, int j) {
-         if (cached[i][j] != null) return cached[i][j];
-        
         // both of i and j is out of bound. exactly matched.
         if (i >= sLen && j >= pLen) return true;
         // j is out of bound. i is inside of bound. not matched. aa, a
@@ -31,16 +25,14 @@ class Solution {
         
         boolean match = i < sLen && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
         
-        // if next character is '*'
+        // next character is '*'
         if (j + 1 < pLen && p.charAt(j + 1) == '*') {
-            cached[i][j] = dpTopDown(i, j + 2) || // not use *
-                (match && dpTopDown(i + 1, j)); // use 
-        } else if (match) {
-            cached[i][j] = dpTopDown(i + 1, j + 1);
-        } else {
-            cached[i][j] = false;
+            return dpTopDown(i, j + 2) || // not use *
+                    (match && dpTopDown(i + 1, j)); // use
         }
         
-        return cached[i][j];
+        if (match) return dpTopDown(i + 1, j + 1);
+        
+        return false;
     }
 }
