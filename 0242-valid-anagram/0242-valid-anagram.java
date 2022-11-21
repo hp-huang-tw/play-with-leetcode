1383e6405ce134e5a54fa5f0d9abd9c71cf4a942
hp.huang <stitchmos@gmail.com>
2022-11-21 12:38:34 +0800

    Time: 26 ms (27.26%), Space: 43.7 MB (60.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        return counter(s, t);
+        return counterPractice2(s, t);
     }
     
-    // TC: O(s+t) -> O(n)
-    // SC: O(s+t) -> O(n)
-    private boolean hashMap(String s, String t) {
+    private boolean counterPractice2(String s, String t) {
         if (s.length() != t.length()) return false;
         
-        HashMap<Character, Integer> mapS = new HashMap<Character, Integer>();
-        HashMap<Character, Integer> mapT = new HashMap<Character, Integer>();
+        int[] counterS = new int[26];
+        int[] counterT = new int[26];
         
         for (int i = 0; i < s.length(); i++) {
-            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i), 0) + 1);
-            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i), 0) + 1);
+            counterS[s.charAt(i) - 'a']++;
+            counterT[t.charAt(i) - 'a']++;
         }
         
-        return mapS.equals(mapT);
+        for (int i = 0; i < 26; i++) {
+            if (counterS[i] != counterT[i]) return false;
+        }
+        
+        return true;
+        
     }
     
     // TC: O(nlog(n)) or O(n^2)
@@ -29,6 +32,8 @@ class Solution {
         return Arrays.equals(cs, ct);
     }
     
+    // TC: O(sLen+26) -> O(n)
+    // SC: O(26*2) -> O(1)
     private boolean counter(String s, String t) {
         if (s.length() != t.length()) return false;
         
@@ -46,5 +51,22 @@ class Solution {
             }
         }
         return true;
+    }
+    
+    // solution for followup question
+    // TC: O(s+t) -> O(n)
+    // SC: O(s+t) -> O(n)
+    private boolean hashMap(String s, String t) {
+        if (s.length() != t.length()) return false;
+        
+        HashMap<Character, Integer> mapS = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> mapT = new HashMap<Character, Integer>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i), 0) + 1);
+            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        
+        return mapS.equals(mapT);
     }
 }
