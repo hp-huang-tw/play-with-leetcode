77994f4f228775c217be7dfbac5d3b1f61623da5
hp.huang <stitchmos@gmail.com>
2022-11-29 18:36:31 +0800

    Time: 8 ms (24.72%), Space: 42.3 MB (39.72%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != ']') {
+                stack.add(c);
+            } else {
+                StringBuilder sb = new StringBuilder();
+                while (stack.peek() != '[') {
+                    sb.insert(0, stack.pop());
+                }
+
+                stack.pop();
+
+               StringBuilder count = new StringBuilder();
+                while(!stack.isEmpty() && Character.isDigit(stack.peek())) {
+                    Character digit = stack.pop();
+                    count.insert(0, digit); // 10[a]
+                }
+
+                for (int j = 0; j < Integer.parseInt(count.toString()); j++) {
+                    sb.chars().forEach(ch -> stack.push((char) ch));
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
