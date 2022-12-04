abb44a72926e282c8f28a5a4d76586e6748ae18c
hp.huang <stitchmos@gmail.com>
2022-12-04 17:18:49 +0800

    Time: 24 ms (43.39%), Space: 86.9 MB (29.58%) - LeetHub

---
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    // naive solution O(n^2)
+    // 2 for-loop to check whether current building is largest one.
+    
+    //                        // canSeeBuildings, maxHeight
+    // heights = [4,2,3,1]    // 
+    //                  ^     // [],                -1
+    //                ^       // [3],                1
+    //              ^         // [3, 2],             3
+    //            ^           // [3, 2],             3
+    //          ^             // [3, 2, 1],          4
+    // then reverse canSeeBuildings
+    
+    // TC: O(n)
+    // SC: O(n)
+    public int[] findBuildings(int[] heights) {
+        int maxHight = -1;
+        
+        // store index
+        List<Integer> canSeeBuildings = new ArrayList<>();
+        
+        for (int i = heights.length - 1; i > -1; i--) {
+            int currHight = heights[i];
+            if (currHight > maxHight) {
+                canSeeBuildings.add(i);
+                maxHight = currHight;
+            }
+        }
+        
+        // reverse index
+        Collections.reverse(canSeeBuildings);
+        return canSeeBuildings.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
