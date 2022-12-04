a4d4ba90d79e22af8abc1b69c9a04b3d8443b310
hp.huang <stitchmos@gmail.com>
2022-12-04 21:27:29 +0800

    Time: 25 ms (40.20%), Space: 68.2 MB (20.66%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,23 +1,38 @@
 class Solution {
+    
+    // TC: (m*n)
+    // SC: (m*n)
     public int maximalSquare(char[][] matrix) {
-        int rows = matrix.length; 
+        int rowLen = matrix.length; 
         // int cols = rows > 0 ? matrix[0].length : 0; // if 0 <= m, n <= 300
-        int cols = matrix[0].length; // the constraints is 1 <= m, n <= 300
+        int colLen = matrix[0].length; // the constraints is 1 <= m, n <= 300
         
-        int[][] dp = new int[rows + 1][cols + 1];
-        int maxSquareLen = 0;
-        // for convenience, we add an extra all zero column and row
-        // outside of the actual dp table, to simpify the transition
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                if (matrix[i-1][j-1] == '1'){
-                    dp[i][j] = 1 + Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), 
-                                        dp[i - 1][j - 1]);
-                    maxSquareLen = Math.max(maxSquareLen, dp[i][j]);
+        // 1. check if self is 1. 
+        // 2. yes. check anti-diagonal, top, left len
+        // original        dp map
+        //                  0 0 0
+        // 0 1 1          0 0 1 1
+        // 1 1 1      ->  0 1 2 2  max = 2 -> area = max * max
+        // 1 0 0          0 1 0 0
+        
+        // we add an extra all zero column and row outside of the actual dp table
+        // to handle out of bound case
+        int[][] dp = new int[rowLen + 1][colLen + 1];
+
+        int maxSqLen = 0;
+        
+        for (int i = 0; i < rowLen; i++) {
+            for (int j = 0; j < colLen; j++) {
+                if (matrix[i][j] == '1') {
+                    // find the min left, top, antiDiag
+                    dp[i + 1][j + 1] = 1 + Math.min(Math.min(dp[i + 1][j], dp[i][j + 1]),
+                                                    dp[i][j]);
+                    maxSqLen = Math.max(maxSqLen, dp[i + 1][j + 1]);
                 }
             }
         }
-        return maxSquareLen * maxSquareLen;
+        
+        return maxSqLen * maxSqLen;
     }
     
     // TC: O((m*n)^2), SC: O(1)
