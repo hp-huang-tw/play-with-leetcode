6fd5736c272cf0976acc94c6233dc9ff3ba59daa
hp.huang <stitchmos@gmail.com>
2022-12-04 16:03:57 +0800

    Time: 62 ms (13.16%), Space: 49.2 MB (15.94%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,7 +1,60 @@
 class Solution {
     
-    // TC: O(n), SC: O(1)
+    // basic template. could extend for
+    // - https://leetcode.com/problems/basic-calculator/
+    // - https://leetcode.com/problems/basic-calculator-iii/
+    // stack solution
     public int calculate(String s) {
+        int strLen = s.length();
+        Stack<Integer> stack = new Stack();
+        int sign = '+';
+        int num = 0;
+        for (int i = 0; i < strLen; i++) {
+            char c = s.charAt(i);
+            
+            // 11
+            //  ^      -> sign = '+', num = 1 * 10 + 1 = 11
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            
+            // 11 - 1 + 2 
+            //        ^      -> sign = '-', num = 1
+            // 11, -1 + 2
+            //          ^    -> sign = '+', num = 2
+            if (c == '+' || c == '-' || c == '*' || c == '/' || i == strLen - 1) {
+                switch (sign) {
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(-num);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * num);
+                        break;
+                    case '/':
+                        stack.push(stack.pop() / num);
+                        break;
+                    default:
+                        break;
+                }
+                
+                num = 0;
+                sign = c;
+            }
+        }
+        
+        
+        
+        int res = 0;
+        while (!stack.isEmpty()) res += stack.pop();
+        return res;
+        // return stack.stream().mapToInt(Integer::intValue).sum();
+    }
+    
+    // TC: O(n), SC: O(1)
+    public int calculateSolution2(String s) {
         int strLength = s.length();
         int res = 0, lastNumber = 0, currentNumber = 0;
         char operation = '+';
