278758cdba6cfa53fcdcb325f640a897341519cd
hp.huang <stitchmos@gmail.com>
2022-12-05 16:10:25 +0800

    Time: 4 ms (97.04%), Space: 43.1 MB (85.60%) - LeetHub

--- 0254-factor-combinations/0254-factor-combinations.java
+++ 0254-factor-combinations/0254-factor-combinations.java
@@ -6,15 +6,19 @@ class Solution {
     
 
     public List<List<Integer>> getFactors(int n) {
-        helper(n, 2);
+        helper2(n, 2);
         return result;
     }
 
-    // n                factors
-    // 12               [2]
-    // 12/2=6           [2, 2]
-    // 6/2=3            [2, 2, ]  
-    //          
+    // n        start     i          factors              
+    //                                            /                   \ 
+    // 12         2       2                      [2]                  [3]
+    //                                     /      |       \            /
+    // r1) 12/2=6 2       2             [2, 2]  [2, 3]  [2, 6]      [3, 4]
+    //                                  /         |           
+    // r2) 6/2=3  2->3   2->3    [2, 2, 3]      [2, 3, 2]    
+    //`r3) 3/3=1  3       3      add factors to result[[2, 2, 3], ]       
+
     public void helper(int n, int start){
         if (n <= 1) {
             if (factors.size() > 1) {
@@ -25,11 +29,35 @@ class Solution {
 
         for (int i = start; i <= n; i++) {
             if (n % i == 0) {
-                factors.add(i);  // [2, 
+                factors.add(i);
                 helper(n/i, i);
                 factors.remove(factors.size() - 1); 
             }
         }
     }
     
+    
+    private void helper2(int n, int start) {
+        if (n == 1) {
+        	if (factors.size() > 1) {
+        		result.add(new ArrayList<>(factors));
+        	}
+            return;
+        }
+	        
+        
+        for (int i = start; i <= (int) Math.sqrt(n); i++) {  // ==> here, change 1
+            if (n % i != 0) {
+                continue;
+            }   
+            factors.add(i);
+            helper2(n/i, i);
+            factors.remove(factors.size()-1);
+        }
+        
+        int i = n; // ===> here, change 2
+        factors.add(i);
+        helper2(n/i, i);
+        factors.remove(factors.size()-1);
+    }
 }
