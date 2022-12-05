c05d9b1354d99d2a749fc1ccdb7e6370cb98758d
hp.huang <stitchmos@gmail.com>
2022-12-05 10:25:42 +0800

    Time: 4 ms (84.22%), Space: 42.2 MB (90.32%) - LeetHub

--- 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -7,8 +7,33 @@ class Solution {
     //       ->       /   \                          ->     /   \
     //              ( )   ( )                             (#)   ( )
     
-    // TC: O(n^2). SC: O(n^2)
+    // iterate string
+    // // TC: O(n). SC: O(1)
     public boolean isValidSerialization(String preorder) {
+        int strLen = preorder.length();
+        
+        int slots = 1;
+        for (int i = 1; i < strLen; i++) {
+            char curr = preorder.charAt(i);
+            
+            if (curr == ',') {
+                slots--;
+                
+                if (slots < 0) return false;
+                
+                if (preorder.charAt(i - 1) != '#') slots += 2;
+            }
+        }
+        
+        // handle last node since the no ',' after it.
+        slots += (preorder.charAt(strLen - 1) == '#') ? -1 : 1;
+        
+        return slots == 0;
+    }
+    
+    // iterate nodes
+    // TC: O(n). SC: O(n)
+    public boolean loopNodes(String preorder) {
         String[] nodes = preorder.split(",");
         
         int slots = 1;
