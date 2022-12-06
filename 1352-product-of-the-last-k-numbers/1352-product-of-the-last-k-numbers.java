ebeab05bcddc0bab8bb6badf4e1c0081cf4e5868
hp.huang <stitchmos@gmail.com>
2022-12-06 13:30:28 +0800

    Time: 23 ms (77.77%), Space: 76.5 MB (26.51%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.java
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.java
@@ -13,7 +13,15 @@ class ProductOfNumbers {
             add(1);
         }};
     }
-
+    
+    // add
+    // input                prefixs     handle zero input issue
+    // [3]                  1,3         
+    // [3,0]                1,3,0       -> 1
+    // [3,0,2]              1,3,0,0     -> 1,2
+    // [3,0,2,5]                           1,2,10
+    // [3,0,2,5,4]                         1,2,10,40
+    // [3,0,2,5,4,8]                       1,2,10,40,320
     public void add(int num) {
         if (num > 0)
             prefixs.add(prefixs.get(prefixs.size() - 1) * num);
@@ -22,9 +30,19 @@ class ProductOfNumbers {
         }
     }
 
+    // [3,0,2,5,4],                         1,2,10,40           (n=4)
+    //                                      k=2 , 40/2 = 20
+    //                                      k=3 , 40/1 = 40
+    //    ^                                 k=4 , 40/1 = 40
+    
+    // [3,0,2,5,4,8]                        1,2,10,40,320
+    //                                      k=2 , 320/10 = 32
+    
+    // [3,0,2,5,4,8]                        1,2,10,40,320 (n=5)
+    //    ^                                 k=5 , 320/1 = 32
     public int getProduct(int k) {
         int n = prefixs.size();
-        return k < n ? prefixs.get(n - 1) / prefixs.get(n - k - 1)  : 0;
+        return (k >= n) ? 0 : prefixs.get(n - 1) / prefixs.get(n - k - 1);
     }
     
     
