437ed08e036096c05867b517836874b9041c5314
hp.huang <stitchmos@gmail.com>
2022-12-10 19:44:28 +0800

    Time: 12 ms (19.04%), Space: 57.5 MB (64.78%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,6 +4,34 @@ class Solution {
         return prefixPostfix(nums);
     }
     
+    // result[i]=prefixs[i-1]*postfixs[i+1].
+    // nums       [1, 2, 3, 4]
+    //              \   \  \
+    // result      1  1. 2. 6      update prefix[i] to res[i+1]
+    //             /  /  /  /
+    // result.     24 12 8  6      then update postfix[i] * res[i-1] to res[i-1]
+ 
+    // TC: O(n). SC:(1)
+    private int[] prefixPostfixOptimization(int[] nums) {
+        int nLen = nums.length;
+        int[] res = new int[nLen];
+        
+        // update prefix[i] to res[i+1]
+        int pre = 1;
+        for (int i = 0; i < nLen - 1; i++) {
+            res[i + 1] = pre * nums[i];
+            pre = res[i + 1];
+        }
+        
+        // update postfix[i] * res[i-1] to res[i-1]
+        int post = 1;
+        for (int i = nLen - 1; i > 0; i--) {
+            post = post * nums[i];
+            res[i - 1] = post * res[i - 1];
+        }
+        
+        return res;
+    }
     
     // result[i]=prefixs[i-1]*postfixs[i+1].
     // nums       [1, 2, 3, 4]
