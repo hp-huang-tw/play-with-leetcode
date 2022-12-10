d7e27c83ed384fcf4e099d7a0341e0587a971acf
hp.huang <stitchmos@gmail.com>
2022-12-10 14:55:27 +0800

    Time: 41 ms (59.90%), Space: 69.6 MB (70.55%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,6 +1,39 @@
 class Solution {
+    
+    int[] nums;
+    
+    int indexK;
+    
     public int findKthLargest(int[] nums, int k) {
-        return minMeap(nums, k);
+        // return minMeap(nums, k);
+        this.nums = nums;
+        int len = nums.length;
+        indexK = len - k;
+        return quickSelectHelper(0, len -1);
+    }
+    
+    // 1 3 2 6 5 4
+    // l         r
+    //          pivot
+    private int quickSelectHelper(int l, int r) {
+        int pivot = nums[r], p = l;
+        for (int i = l; i < r; i++) {
+            if (nums[i] <= pivot) {
+                swap(p, i);
+                p++;
+            }
+        }
+        swap(p, r);
+        
+        if (p > indexK) return quickSelectHelper(l, p - 1);
+        else if (p < indexK) return quickSelectHelper(p + 1, r);
+        else return nums[p];
+    }
+    
+    private void swap(int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     
     // sort is O(nLog(n))
