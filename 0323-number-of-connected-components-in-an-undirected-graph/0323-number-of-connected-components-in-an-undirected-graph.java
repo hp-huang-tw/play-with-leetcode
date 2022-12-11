8b5518b340b54b94e4454debdecf0d8c34a30921
hp.huang <stitchmos@gmail.com>
2022-12-11 22:40:37 +0800

    Time: 5 ms (71.76%), Space: 46.3 MB (64.83%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -13,8 +13,8 @@ class Solution {
         size = new int[n]; // size of component
         
         for (int i = 0; i < n; i++) {
-            parent[i] = i;
-            size[i] = 1;
+            parent[i] = i;  // init self as its parent
+            size[i] = 1;    // init size as 1
         }
         
         int componentCount = n;
