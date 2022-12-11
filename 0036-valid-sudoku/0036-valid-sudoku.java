bc9959589cb9a9d55b9a41befd3c3cf767ddbdf7
hp.huang <stitchmos@gmail.com>
2022-12-11 14:30:32 +0800

    Time: 7 ms (57.94%), Space: 48.3 MB (20.88%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,68 @@
+class Solution {
+    /*
+    [["5","3",".",".","7",".",".",".","."]
+    ,["6",".",".","1","9","5",".",".","."]
+    ,[".","9","8",".",".",".",".","6","."]
+    ,["8",".",".",".","6",".",".",".","3"]
+    ,["4",".",".","8",".","3",".",".","1"]
+    ,["7",".",".",".","2",".",".",".","6"]
+    ,[".","6",".",".",".",".","2","8","."]
+    ,[".",".",".","4","1","9",".",".","5"]
+    ,[".",".",".",".","8",".",".","7","9"]]
+             r = 0
+    3 3 3   0, 0 (0-2, 0-2) | 0, 1 (0-2, 3-5) | 0, 2 (0-2, 6-8)
+    3 3 3   1, 0 (3-5, 0-2) | 1, 1 (3-5, 3-5) | 1, 2 (3-5, 6-8)
+    3 3 3   2, 0 (6-8, 0-2) | 2, 1 (6-8, 3-5) | 2, 2 (6-8, 6-8)
+    */
+    
+
+
+    public boolean isValidSudoku(char[][] board) {
+        int n = 9;
+        
+        HashSet<Character>[] rows = new HashSet[n];
+        HashSet<Character>[] cols  = new HashSet[n];
+        HashSet<Character>[] boxes = new HashSet[n];
+        
+        for (int i = 0; i < n; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+            
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                char val = board[r][c];
+                
+                if (val == '.') continue;
+                
+                // check rows
+                if (rows[r].contains(val)) {
+                    return false;
+                }
+                
+                rows[r].add(val);
+                
+                // check cols
+                if (cols[c].contains(val)) {
+                    return false;
+                }
+                cols[c].add(val);
+                
+                
+                // check boxes
+                // r/3 012(0) 345(1) 678 (2)   0+0     0+1     0+2
+                // c/3 012(0) 345(1) 678 (2)   1*3+0   1*3+1   1*3+2 
+                //                             2*3+0   2*3+1   2*3+2
+                int boxIdx = (r / 3) * 3 + (c / 3);
+                if (boxes[boxIdx].contains(val)) {
+                    return false;
+                }
+                
+                boxes[boxIdx].add(val);
+            }
+        }
+        
+        return true;
+    }
+}
