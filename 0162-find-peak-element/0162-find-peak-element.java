84b2deaca7648cfeaf3082ddaf5e03ca64c3c505
hp.huang <stitchmos@gmail.com>
2022-12-11 22:12:05 +0800

    Time: 0 ms (100.00%), Space: 42.1 MB (78.48%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,6 +1,17 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        return linearScan(nums);
+        return binarySearch(nums, 0, nums.length - 1);
+    }
+    
+    private int binarySearch(int[] nums, int l, int r) {
+        if (l == r) 
+            return l;
+        
+        int mid = (l + r) / 2;
+        if (nums[mid] > nums[mid + 1]) {
+            return binarySearch(nums, l, mid);
+        }
+        return binarySearch(nums, mid + 1, r);
     }
     
     private int linearScan(int[] nums) {
