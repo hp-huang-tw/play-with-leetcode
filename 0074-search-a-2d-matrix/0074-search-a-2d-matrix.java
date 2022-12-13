b2cc29f9a8d60ef614154b2b07465535b1dc553c
hp.huang <stitchmos@gmail.com>
2022-12-13 17:47:13 +0800

    Time: 0 ms (100.00%), Space: 42.4 MB (64.53%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -7,7 +7,7 @@ class Solution {
             matrix[mid/c][mid%c]
     */
     // TC: o(log(mn)). SC: O(1)
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix_binary(int[][] matrix, int target) {
         int rowLen = matrix.length;
         if (rowLen == 0) return false;
         
@@ -26,6 +26,26 @@ class Solution {
                 l = mid + 1;
             }
         }
+        return false;
+    }
+    
+    // TC: O(r+c), O(1) searchMatrix II
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rowLen = matrix.length;
+        int colLen = matrix[0].length;
+        
+        int r = rowLen - 1, c = 0;
+        while (r > -1 && c < colLen) {
+            int value = matrix[r][c];
+            if (value > target) {
+                r--;
+            } else if (value < target) {
+                c++;
+            } else {
+                return true;
+            }
+        }
+        
         return false;
     }
 }
