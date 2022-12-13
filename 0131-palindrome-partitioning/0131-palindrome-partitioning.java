9998f507754b4c4dbad62bbd34dc630beb9c6356
hp.huang <stitchmos@gmail.com>
2022-12-13 11:19:48 +0800

    Time: 9 ms (97.55%), Space: 54.9 MB (86.55%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,57 @@
+class Solution {
+    
+    String s;
+    
+    int strLen;
+    
+    List<List<String>> res = new ArrayList<>();
+    
+    // backtracking -> dfs -> recursive
+    /*
+                    aab
+                 /   |    \
+                a.   aa     abb x
+              /      |          
+            a        b
+           /       
+         b
+    */
+   
+    public List<List<String>> partition(String s) {    
+        dfs(s, 0, new ArrayList<>());  
+        return res;
+    }
+    
+     // TC: O(2^n) * O(n)
+    private void dfs(String s, int start, List<String> vaildPartitions) {
+        int strLen = s.length();
+        if (start >= strLen) {
+            res.add(new ArrayList<>(vaildPartitions));   // deep copy
+            return;
+        }
+        
+        for (int j = start; j < strLen; j++) {                          //      a -> aa -> aab
+            String partition = s.substring(start, j + 1);               //     /      |
+            if (isPalindrone(partition)) {                              //.   |       |
+                vaildPartitions.add(partition);                         //    |       |
+                dfs(s, j + 1, vaildPartitions);                         //    a       b           
+                vaildPartitions.remove(vaildPartitions.size() - 1);     //.   b
+            }
+        }
+        
+    }
+    
+    private boolean isPalindrone(String p) {
+        int l = 0, r = p.length() - 1;
+        
+        while (l < r) {
+            if (p.charAt(l) != p.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        
+        return true;
+    }
+}
