1b0f038c253c320c5e990be160ffdf40a2302d7f
hp.huang <stitchmos@gmail.com>
2022-12-14 18:08:02 +0800

    Time: 0 ms (100.00%), Space: 40.1 MB (94.98%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -4,24 +4,36 @@ class Solution {
         // i: 0-len2-1. if (not macthed@1[curr]) move start = 1(curr) + 1;
         // j: 0-len2-1
         // empty needle appears everywhere, first appears at 0 index
-        if (needle.length() == 0)
+        int len1 = haystack.length(), len2 = needle.length();
+        
+        if (len2 == 0) { 
             return 0;
-        if (haystack.length() == 0)
+        }
+        
+        if (len1 == 0) {
             return -1;
+        }
         
-        
-        for (int i = 0; i < haystack.length(); i++) {
-            // no enough places for needle after i
-            if (i + needle.length() > haystack.length()) break;
-            
-            for (int j = 0; j < needle.length(); j++) {
-                if (haystack.charAt(i+j) != needle.charAt(j))
-                    break;
-                if (j == needle.length()-1)
-                    return i;
+        // sadbutsad    // sadbutsad    // sadbutsad
+        // i               i               i
+        // sad          // sad             sad
+        // j                j                j <---- exactly matched
+        int count = 0;
+        for (int start = 0; start < len1; start++) { 
+            for (int j = 0; j < len2; j++) {   
+                if (start + j > len1 - 1) break;
+                
+                if (needle.charAt(j) != haystack.charAt(start + j)) break;                
+                if (j == (len2 -1)) {
+                    return start;
+                }
+                
+
             }
         }
         
         return -1;
+        
+        
     }
 }
