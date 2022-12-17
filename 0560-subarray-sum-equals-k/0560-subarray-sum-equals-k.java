cd638954c9a23a74e7d8870a44316afadb8b94b0
hp.huang <stitchmos@gmail.com>
2022-12-17 13:58:00 +0800

    Time: 21 ms (97.81%), Space: 47 MB (78.06%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,22 +1,37 @@
-class Solution {
+class Solution {                                                       
+    //       [1, -1, 1, 1, 1, 1],     k = 3                    
+    //       [1]        [1, -1]    [1, -1, 1]   [1, -1, 1, 1]   [1, -1, 1, 1, 1]        [1, -1, 1, 1, 1, 1]                    
+    // sum    1          0             1              2               3                       4
+    //.      1-3=-2     0-3 = -3     1-3=-2         2-3= -1         3-3=0                   4-3=1
+    // res    0                                                       2                       4
+    
+    /*
+        preSums count
+-       ---------------
+         0       1->2       [ ], [1,-1]
+         1       1->2       [1], [1,-1,1]   
+         2.      1          [1,-1,1,1]
+         3.      1          [1,-1,1,1,1]
+    */
     
     // TC: O(n), SC: O(n)
     public int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> preSums = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> preSums = new HashMap<>();
         preSums.put(0, 1);
-        int count = 0;
-        int sum = 0;
         
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
+        int sum = 0;
+        int res = 0;
+        
+        for (int n : nums) {
+            sum += n;
             
-            if (preSums.containsKey(sum-k)) {
-                count += preSums.get(sum-k);
+            if (preSums.containsKey(sum - k)) {
+                res += preSums.get(sum - k);
             }
             
-            preSums.put(sum, preSums.getOrDefault(sum, 0) +1);
+            preSums.put(sum, 1 + preSums.getOrDefault(sum, 0));
         }
         
-        return count;
+        return res;
     }
 }
