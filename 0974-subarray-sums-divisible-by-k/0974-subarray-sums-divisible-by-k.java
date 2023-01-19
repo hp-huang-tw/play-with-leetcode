0d4d601a51e890ba26d4cf19c05d79ff2305713f
hp.huang <stitchmos@gmail.com>
2023-01-19 23:40:13 +0800

    Time: 26 ms (44.22%), Space: 46.8 MB (69.94%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,14 +1,20 @@
 class Solution {
     /*
         [4,5,0,-2,-3,1]     sum     sum%5   res     proMod
-        [4]                 4       4       0      {0,1}{4,1}       [];[4]
-        [4,5]            v  9       4       1      {0,1}{4,1}{9,1}  [];[4];[4,5]
-        [4,5,0]          v  9       4       1+2
-        [4,5,0,-2]          7       2       3      {0,1}{4,1}{9,1}{7,1}
-        [4,5,0,-2,-3]    v  9       4       3+3
+        [4]                 4       4       0       {0,1}{4,1}          
+                                                    [];[4]
+        [4,5]            v  9       4       1       {0,1}{4,2}          
+                                                    [];[4]
+        [4,5,0]          v  9       4       1+2     {0,1}{4,3}          
+                                                    [];[4],[4,5],[4,5,0]
+        [4,5,0,-2]          7       2       3       {0,1}{4,3}{2,1}     
+                                                    [];[4],[4,5],[4,5,0];[4,5,0,-2]
+        [4,5,0,-2,-3]    v  9       4       3+3     {0,1}{4,4}{2,1}     
+                                                    [];[4],[4,5],[4,5,0],[4,5,0,-2,-3];[4,5,0,-2]
         [4,5,0,-2,-3,1]     10      0
-        
-        
+    */
+    /*   
+    result
             [5], 
             -------------------
             [5, 0], 
@@ -20,14 +26,17 @@ class Solution {
             [5, 0, -2, -3], 
     */
     public int subarraysDivByK(int[] nums, int k) {
-        int[] map = new int[k];
-		map[0] = 1;
-        int res = 0, sum = 0;
-        for(int n : nums) {
-            sum = (sum + n) % k;
-            if(sum < 0) sum += k;  // Because -1 % 5 = -1, but we need the positive mod 4
-            res += map[sum];
-            map[sum]++;
+        HashMap<Integer, Integer> preMods = new HashMap<>();
+        preMods.put(0, 1);
+        
+        int sum = 0, res = 0, mod = 0;
+        for (int n : nums) {
+            sum += n;
+            mod = (sum % k) < 0 ? (sum % k) + k: sum % k;
+            if (preMods.containsKey(mod)) {
+                res += preMods.get(mod);
+            }
+            preMods.put(mod, 1 + preMods.getOrDefault(mod, 0));
         }
         return res;
     }
