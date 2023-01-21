7829d9b972f3c96345478de4ec120f253d15d6cf
hp.huang <stitchmos@gmail.com>
2023-01-21 22:12:04 +0800

    Time: 3 ms (70.75%), Space: 48.2 MB (57.59%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -19,11 +19,11 @@ class Solution {
     // TC: O(n^2 * log(n^2))
     // SC: O(1)
     
-    public int kthSmallest(int[][] matrix, int k) {
+    public int kthSmallest2(int[][] matrix, int k) {
         int rows = matrix.length, cols = matrix[0].length;
         
         int lo = matrix[0][0], hi = matrix[rows - 1][cols - 1] ;
-        while(lo <= hi) {
+        while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
             int count = 0,  maxNum = lo;
            
@@ -34,7 +34,7 @@ class Solution {
                     count += (c + 1); // count of nums <= mid in matrix
                     maxNum = Math.max(maxNum, matrix[r][c]); 
          // mid might be value not in  matrix, we need to record the actually max num;
-                }else{ //it means c < 0
+                } else { //it means c < 0
                     break;
                 } 
             }
@@ -47,22 +47,31 @@ class Solution {
         return lo;
     }
     
-    public int kthSmallest2(int[][] matrix, int k) {
+    /*
+    [[1,1,3,8,13],
+     [4,4,4,8,18],
+     [9,14,18,19,20],
+     [14,19,23,25,25],
+     [18,21,26,28,29]]
+     13
+    */
+    public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
         int l = matrix[0][0], r = matrix[n-1][n-1];
         
-        while (l < r) {
-            int mid = (l + r)/2;
+        while (l <= r) {
+            int mid = (l+r)/2;
             
             // count of numbers which is less and equal to mid
             int count = 0;
             int max = Integer.MIN_VALUE;    // the max value of numbers which is less and equal to mid
             for (int i = 0; i < n; i++) {
-                
                 for (int j = 0; j < n; j++) {
                     if (matrix[i][j] <= mid) {
                         count++;
                         max = Math.max(max, matrix[i][j]);
+                    } else {
+                        break;
                     }
                 }
             }
@@ -77,9 +86,7 @@ class Solution {
             }
         }
         
-        return r;
+        return l;
     }
     
-    
-
 }
