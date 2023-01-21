6a5a50fd78683ecfc3061bf3e363c1f5c634aca4
hp.huang <stitchmos@gmail.com>
2023-01-21 22:12:35 +0800

    Time: 3 ms (70.75%), Space: 48.3 MB (52.54%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -18,35 +18,6 @@ class Solution {
     
     // TC: O(n^2 * log(n^2))
     // SC: O(1)
-    
-    public int kthSmallest2(int[][] matrix, int k) {
-        int rows = matrix.length, cols = matrix[0].length;
-        
-        int lo = matrix[0][0], hi = matrix[rows - 1][cols - 1] ;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            int count = 0,  maxNum = lo;
-           
-            for (int r = 0, c = cols - 1; r < rows; r++) {
-                while (c >= 0 && matrix[r][c] > mid) c--;   
-                
-                if (c >= 0) {
-                    count += (c + 1); // count of nums <= mid in matrix
-                    maxNum = Math.max(maxNum, matrix[r][c]); 
-         // mid might be value not in  matrix, we need to record the actually max num;
-                } else { //it means c < 0
-                    break;
-                } 
-            }
-            
-            // adjust search range
-            if (count == k) return maxNum;
-            else if (count < k) lo = mid + 1;
-            else hi = mid - 1;
-        }
-        return lo;
-    }
-    
     /*
     [[1,1,3,8,13],
      [4,4,4,8,18],
@@ -60,7 +31,7 @@ class Solution {
         int l = matrix[0][0], r = matrix[n-1][n-1];
         
         while (l <= r) {
-            int mid = (l+r)/2;
+            int mid = (l + r)/2;
             
             // count of numbers which is less and equal to mid
             int count = 0;
