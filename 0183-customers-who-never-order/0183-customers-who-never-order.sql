fa93e519a274d9cff2ab4ab33498698b82fac402
hp.huang <stitchmos@gmail.com>
2023-02-12 14:12:53 +0800

    Time: 960 ms (39.23%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -6,9 +6,9 @@
 # WHERE id NOT IN (SELECT customerId FROM Orders);
 
 # OPTIMIZE
-# SELECT name AS `Customers` FROM Customers c
-# WHERE NOT EXISTS (SELECT 1 FROM Orders o WHERE o.customerId = c.id);
+SELECT name AS `Customers` FROM Customers c
+WHERE NOT EXISTS (SELECT 1 FROM Orders o WHERE o.customerId = c.id);
 
-SELECT c.name AS `Customers` FROM Customers c
-LEFT JOIN Orders o ON c.id = o.customerId
-WHERE o.id IS NULL;
+# SELECT c.name AS `Customers` FROM Customers c
+# LEFT JOIN Orders o ON c.id = o.customerId
+# WHERE o.id IS NULL;
