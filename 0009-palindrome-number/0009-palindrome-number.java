ec900ac9215946dc5129727453706f00c8a9d667
hp.huang <stitchmos@gmail.com>
2023-02-12 22:46:38 +0800

    Time: 10 ms (53.87%), Space: 41.9 MB (49.55%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    // O(n), O(1)
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        
+        return true;
+    }
+}
