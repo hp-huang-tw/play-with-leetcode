7d25af6c1ecc613d8cdc8d09499bf9cda863aac8
hp.huang <stitchmos@gmail.com>
2023-02-13 11:38:31 +0800

    Time: 2 ms (58.54%), Space: 40.2 MB (81.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -13,18 +13,16 @@ class Solution {
     // ..
     // ""     return true
     public boolean isValid(String s) {
-        Map<Character, Character> map = 
-            Map.of('(', ')', '{', '}', '[', ']');
+        Map<Character, Character> map = Map.of('(', ')', '{', '}', '[', ']');
         
         Stack<Character> stack = new Stack<>();
         
         for (char c : s.toCharArray()) {
             if (map.containsKey(c)) {
                 stack.push(c);
+            } else if (stack.isEmpty() || c != map.get(stack.peek())) {  // { | ], // "" | ]
+              return false;
             } else {
-                if (stack.isEmpty() || c != map.get(stack.peek())) {  // { | ], // ]
-                    return false;
-                }
                 stack.pop();
             }
         }
