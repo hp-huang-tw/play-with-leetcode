21ae254fb308a72d4930ad13625773da361f129f
hp.huang <stitchmos@gmail.com>
2023-02-13 11:37:30 +0800

    Time: 2 ms (58.54%), Space: 39.9 MB (95.85%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    // ()
+    // ([{}[]]) 
+    //   ^^^^^
+    // stack    c
+    // (      | (
+    // ([     | [
+    // ([{    | {
+    // ([{    | }   pop {
+    // ([[    | [
+    // ([[    | ]   pop [
+    // ([
+    // ..
+    // ""     return true
+    public boolean isValid(String s) {
+        Map<Character, Character> map = 
+            Map.of('(', ')', '{', '}', '[', ']');
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || c != map.get(stack.peek())) {  // { | ], // ]
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
