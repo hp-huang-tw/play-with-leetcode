69e61c25f07908b435af146318011be3aeac173b
hp.huang <stitchmos@gmail.com>
2023-02-14 15:45:10 +0800

    Time: 0 ms (100.00%), Space: 40.5 MB (92.46%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,29 +1,29 @@
 class Solution {
     /*
         [3, 2, 1, 3] val = 3
-         l        r
+         l          r
          3. 2. 1  3
-         l     r
+         l        r
                r
          1  2. 3. 3
             l
-            r
+               r
             
         [0,1,2,2,3,0,4,2], val = 2
-         l             r
+         l               r
              l
              2 2 3 0 4 2
-             l       r
+             l         r
              4 2 3 0 2
-             l     r
+             l       r
                l
                0 3 2 2
-               l.  r
+               l.    r
                  l 
                    l
                r.  l.  return l
         [0]
-         l.r
+         l  r
     */
     public int removeElement(int[] nums, int val) {
         int l = 0, r = nums.length;
