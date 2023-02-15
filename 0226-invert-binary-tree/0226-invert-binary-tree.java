725f70b822ba40aa7fb7ee600d800568b582df17
hp.huang <stitchmos@gmail.com>
2023-02-15 11:31:52 +0800

    Time: 0 ms (100.00%), Space: 39.6 MB (86.06%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -14,17 +14,22 @@
  * }
  */
 class Solution {
+    /*
+            4               4
+          /   \           /    \         
+         2     7        7       2
+        / \.  / \      / \     / \       /.\            ->               / \.
+       1.  3 6   9    6   9   1   3      9  6                           3.  1
+    */
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
-        
-        TreeNode tmp = root.left;
+        TreeNode temp = root.left;
         root.left = root.right;
-        root.right = tmp;
+        root.right = temp;
         
         invertTree(root.left);
         invertTree(root.right);
         
         return root;
     }
-    
 }
