2cdf2b14a6167678e7341170d5bdaa03d9b91cdf
hp.huang <stitchmos@gmail.com>
2023-02-15 23:03:01 +0800

    Time: 1057 ms (52.86%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,9 +1,23 @@
 # Write your MySQL query statement below
+
 # SELECT employee_id, salary * (name NOT LIKE 'M%') * (employee_id  % 2) AS bonus 
 # FROM Employees
 # ORDER BY employee_id;
 
+# SELECT employee_id, IF (name NOT LIKE 'M%' AND employee_id % 2 <> 0, salary, 0) AS bonus 
+# FROM Employees
+# ORDER BY employee_id;
 
-SELECT employee_id, IF (name NOT LIKE 'M%' AND employee_id % 2 <> 0, salary, 0) AS bonus 
-FROM Employees
-ORDER BY employee_id;
+-- select entries with odd  employee id and name not start with M
+    SELECT employee_id , salary AS bonus 
+    FROM employees 
+    WHERE employee_id % 2 <> 0 AND name NOT LIKE 'M%'
+
+-- join both selection 
+    union
+
+-- select remaining entries from table 
+    SELECT employee_id , 0 AS bonus
+    FROM employees
+    WHERE employee_id % 2 = 0 OR name LIKE 'M%'
+    ORDER BY employee_id;
