ee5b7c0f80438cdeaea77418dc1078718f08c2a9
hp.huang <stitchmos@gmail.com>
2023-02-15 12:23:35 +0800

    Time: 591 ms (31.84%), Space: 0B (100.00%) - LeetHub

--- 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
+++ 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
@@ -1,8 +1,13 @@
 # Write your MySQL query statement below
-SELECT class 
-FROM (
-    SELECT class, COUNT(student) AS cnt 
-    FROM Courses 
-    GROUP BY class
-) AS tb
-WHERE cnt >= 5;
+# SELECT class 
+# FROM (
+#     SELECT class, COUNT(student) AS cnt 
+#     FROM Courses 
+#     GROUP BY class
+# ) AS tb
+# WHERE cnt >= 5;
+
+SELECT class
+FROM Courses 
+GROUP BY class
+HAVING COUNT(student) >= 5;
