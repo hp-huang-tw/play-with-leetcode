b918942eb4a22135e0a533e474d3278f15116f72
hp.huang <stitchmos@gmail.com>
2023-04-14 12:45:46 +0800

    Time: 688 ms (71.09%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# Solution 1
 # SELECT * FROM Employee e1
 # LEFT JOIN Employee e2 ON e1.managerId = e2.id;
 
@@ -12,6 +13,39 @@
 # | 4  | Max   | 90000  | Null      |Null| Null  | Null   | Null      |
 # +----+-------+--------+-----------+----+-------+--------+-----------+
 
-SELECT e1.name AS Employee FROM Employee e1
-LEFT JOIN Employee e2 ON e1.managerId = e2.id
-WHERE e1.salary > e2.salary;
+# SELECT e1.name AS Employee FROM Employee e1
+# LEFT JOIN Employee e2 ON e1.managerId = e2.id
+# WHERE e1.salary > e2.salary;
+
+
+# Solution 2
+# SELECT e1.name AS Employee FROM Employee e1, Employee e2;
+
+# | e1                              | e2                              |
+# +----+-------+--------+-----------+----+-------+--------+-----------+
+# | id | name  | salary | managerId | id | name  | salary | managerId |
+# +----+-------+--------+-----------+----+-------+--------+-----------+
+# | 1  | Joe   | 70000  | 3         | 1  | Joe   | 70000  | 3         |
+# | 2  | Henry | 80000  | 4         | 1  | Joe   | 70000  | 3         |
+# | 3  | Sam   | 60000  | Null      | 1  | Joe   | 70000  | 3         |
+# | 4  | Max   | 90000  | Null      | 1  | Joe   | 70000  | 3         |
+# +----+-------+--------+-----------+----+-------+--------+-----------+
+# | 1  | Joe   | 70000  | 3         | 2  | Henry | 80000  | 4         |
+# | 2  | Henry | 80000  | 4         | 2  | Henry | 80000  | 4         |
+# | 3  | Sam   | 60000  | Null      | 2  | Henry | 80000  | 4         |
+# | 4  | Max   | 90000  | Null      | 2  | Henry | 80000  | 4         |
+# +----+-------+--------+-----------+----+-------+--------+-----------+
+# | 1  | Joe   | 70000  | 3         | 3  | Sam   | 60000  | Null      |
+# | 2  | Henry | 80000  | 4         | 3  | Sam   | 60000  | Null      |
+# | 3  | Sam   | 60000  | Null      | 3  | Sam   | 60000  | Null      |
+# | 4  | Max   | 90000  | Null      | 3  | Sam   | 60000  | Null      |
+# +----+-------+--------+-----------+----+-------+--------+-----------+
+# | 1  | Joe   | 70000  | 3         | 4  | Max   | 90000  | Null      |
+# | 2  | Henry | 80000  | 4         | 4  | Max   | 90000  | Null      |
+# | 3  | Sam   | 60000  | Null      | 4  | Max   | 90000  | Null      |
+# | 4  | Max   | 90000  | Null      | 4  | Max   | 90000  | Null      |
+# +----+-------+--------+-----------+----+-------+--------+-----------+
+
+
+SELECT e1.name AS Employee FROM Employee e1,Employee e2
+WHERE e1.managerId = e2.id AND e1.salary > e2.salary; 
