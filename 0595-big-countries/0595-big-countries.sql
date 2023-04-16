604f647e67d9be35b69dfac5cb20d2373d164b70
hp.huang <stitchmos@gmail.com>
2023-04-16 16:03:32 +0800

    update manually

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,1 +1,14 @@
-SELECT name, population, area FROM World WHERE area >= 3000000 or population >= 25000000;
+# UNION - better performance for index creation
+SELECT name, population, area
+FROM World 
+WHERE area >= 3000000
+
+UNION
+
+SELECT name, population, area
+FROM World 
+WHERE population >= 25000000;
+
+# SELECT name, population, area
+# FROM World 
+# WHERE area >= 3000000 OR population >= 25000000;
