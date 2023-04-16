e7dc095e1482fdfa26060c96ea2f7e8103838414
hp.huang <stitchmos@gmail.com>
2023-04-16 16:09:50 +0800

    Time: 1116 ms (27.64%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,14 +1,5 @@
 # Write your MySQL query statement below
-# SELECT name AS `Customers` FROM Customers 
-# WHERE id NOT IN (SELECT DISTINCT customerId FROM Orders);
-
-# SELECT name AS `Customers` FROM Customers 
-# WHERE id NOT IN (SELECT customerId FROM Orders);
-
-# OPTIMIZE
-SELECT name AS `Customers` FROM Customers c
-WHERE NOT EXISTS (SELECT 1 FROM Orders o WHERE o.customerId = c.id);
-
-# SELECT c.name AS `Customers` FROM Customers c
-# LEFT JOIN Orders o ON c.id = o.customerId
-# WHERE o.id IS NULL;
+SELECT name AS Customers
+FROM Customers c
+LEFT JOIN Orders o on c.id = o.customerId
+WHERE o.customerId IS NULL;
