94c8befd6720e9945c784b1fa07b41ac722a41c4
hp.huang <stitchmos@gmail.com>
2023-04-16 15:45:01 +0800

    Time: 1001 ms (37.51%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,2 +1,12 @@
 # Write your MySQL query statement below
-SELECT name FROM Customer WHERE referee_id <> 2 OR referee_id IS NULL;
+
+SELECT name 
+FROM Customer 
+WHERE referee_id IS NULL 
+
+UNION ALL
+
+SELECT name 
+FROM Customer 
+WHERE referee_id <> 2
+;
