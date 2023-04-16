7c7d8090dc9db3bf474a60a5d2ea6419dd2d8a22
hp.huang <stitchmos@gmail.com>
2023-04-16 15:50:44 +0800

    Time: 1102 ms (19.34%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,12 +1,17 @@
 # Write your MySQL query statement below
 
+# SELECT name 
+# FROM Customer 
+# WHERE referee_id IS NULL 
+
+# UNION ALL
+
+# SELECT name 
+# FROM Customer 
+# WHERE referee_id <> 2
+# ;
+
+# query on same coloumn so the following is enough. index is work.
 SELECT name 
 FROM Customer 
-WHERE referee_id IS NULL 
-
-UNION ALL
-
-SELECT name 
-FROM Customer 
-WHERE referee_id <> 2
-;
+WHERE referee_id IS NULL OR referee_id <> 2;
