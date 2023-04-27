4ccb57f49caa7aaa358bf50939853f4d6b867f89
hp.huang <stitchmos@gmail.com>
2023-04-27 11:53:12 +0800

    Time: 908 ms (42.51%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
-SELECT w1.id 
-FROM Weather w1, Weather w2
-WHERE w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY) AND w2.temperature < w1.temperature;
+# SELECT w1.id 
+# FROM Weather w1, Weather w2
+# WHERE w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY) AND w2.temperature < w1.temperature;
+
+
+SELECT w1.id AS' id'
+FROM Weather w1
+JOIN Weather w2 ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+AND w1.temperature > w2.temperature;
+
