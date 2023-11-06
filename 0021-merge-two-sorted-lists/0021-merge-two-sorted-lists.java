23d8b360b998c5a61408581e505ca863f127564c
hp.huang <stitchmos@gmail.com>
2023-11-06 16:29:57 +0800

    Time: 0 ms (100.00%), Space: 41.8 MB (12.54%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -12,21 +12,21 @@ class Solution {
     // O(n+m), O(1)
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
+        ListNode tail = dummy;
         
         while (list1 != null && list2 != null) {
             if (list1.val <= list2.val) {
-                curr.next = list1;
+                tail.next = list1;
                 list1 = list1.next;
             } else {
-                curr.next = list2;
+                tail.next = list2;
                 list2 = list2.next;
             }
             
-            curr = curr.next;
+            tail = tail.next;
         }
         
-        curr.next = list1 == null ? list2 : list1;
+        tail.next = list1 == null ? list2 : list1;
         
         return dummy.next;
     }
@@ -36,7 +36,7 @@ class Solution {
     [0] 
      |   
     [1] [2]  [4]     [null]
-     |  /|  / curr     l1    
+     |  /|  / tail     l1    
      | / | /   |
     [1] [3]   [4]-[null]
               l2
