988e787a183d4b93c36e7ca37973b2ac7d3dd2d4
hp.huang <stitchmos@gmail.com>
2023-11-07 18:03:08 +0800

    Time: 2 ms (99.36%), Space: 41.6 MB (87.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,12 +1,23 @@
 class Solution {
     
-    // O(n), O(n)
+    // O(n), O(1)
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int l = 0, r = s.length() - 1;
         
-        StringBuilder sb = new StringBuilder(s);
-        String reversed = sb.reverse().toString();
-        
-        return s.equals(reversed);
+        while (l < r) {
+            if (!Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
+            } else if (!Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+            } else {
+                if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                    return false;
+                }
+                
+                l++;
+                r--;
+            }
+        }
+        return true;
     }
 }
