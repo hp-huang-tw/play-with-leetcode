46e2729a64e9ba120445b760b4ea5c7adddf40ba
hp.huang <stitchmos@gmail.com>
2023-11-07 17:45:09 +0800

    Time: 21 ms (30.88%), Space: 43.6 MB (48.17%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    
+    // O(n), O(n)
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        
+        StringBuilder sb = new StringBuilder(s);
+        String reversed = sb.reverse().toString();
+        
+        return s.equals(reversed);
+    }
+}
