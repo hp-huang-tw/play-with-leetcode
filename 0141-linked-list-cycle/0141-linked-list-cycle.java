74ed8e1f8989d0e1f508290e307e9bf8e37243ce
hp.huang <stitchmos@gmail.com>
2023-11-16 18:09:03 +0800

    Time: 0 ms (100.00%), Space: 44.4 MB (7.39%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,16 +11,20 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) return false;
+        if (head == null || head.next == null) {
+            return false;
+        } // No cycle if the list is empty or has only one node
         
         ListNode slow = head, fast = head.next;
         
         while (slow != fast) {
-            if (fast == null || fast.next == null) return false;
+            if (fast == null || fast.next == null) {
+                return false;
+            } // No cycle if the fast pointer reaches the end
             slow = slow.next;
             fast = fast.next.next;
         }
         
-        return true;
+        return true; // There is a cycle if the slow and fast pointers meet
     }
 }
