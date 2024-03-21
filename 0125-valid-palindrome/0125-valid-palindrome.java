2c1c7634187656625f288c51251c6e5fb9a02abc
hp.huang <stitchmos@gmail.com>
2024-03-21 23:42:35 +0800

    Time: 3 ms (75.14%), Space: 42.4 MB (96.80%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,19 +1,14 @@
 class Solution {
-    
-    // O(n), O(1)
     public boolean isPalindrome(String s) {
         int l = 0, r = s.length() - 1;
-        
         while (l < r) {
             if (!Character.isLetterOrDigit(s.charAt(l))) {
                 l++;
             } else if (!Character.isLetterOrDigit(s.charAt(r))) {
                 r--;
+            } else if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
             } else {
-                if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
-                    return false;
-                }
-                
                 l++;
                 r--;
             }
@@ -21,3 +16,11 @@ class Solution {
         return true;
     }
 }
+
+
+// a, bcb a
+//       l
+//       r
+
+// //abcxba
+//     lr
