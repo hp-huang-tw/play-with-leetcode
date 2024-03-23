c3294e4c63bc1fa4278c3db270147cb1b98764d9
hp.huang <stitchmos@gmail.com>
2024-03-23 21:24:47 +0800

    Time: 0 ms (100.00%), Space: 44.5 MB (93.50%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,36 +1,32 @@
 class Solution {
-    private static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    
+		// Define directions: top, bottom, left, right
+		private static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+		
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        // Define the target color `originalColor` at the source point (sr, sc). 
+		    // This is the color of the region we want to flood fill.
         int originalColor = image[sr][sc];
         
-        // No need to flood fill if the new color is the same as the original color
-        if (originalColor == color) return image;
-        
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{sr, sc});
-        
-        while (!q.isEmpty()) {
-            int[] currentPosition = q.poll();
-            int r = currentPosition[0];
-            int c = currentPosition[1];
-            
-            // Fill the current cell with the new color
-	        image[r][c] = color; 
-            
-
-			// Explore neighbors in all four directions
-            for (int[] dir : DIRECTIONS) {
-                int newRow = r + dir[0];
-                int newCol = c + dir[1];
-                
-                if (newRow >= 0 && newCol >= 0 && newRow < image.length && newCol < image[0].length && image[newRow][newCol] == originalColor) {
-                    // Add neighboring cell to the queue for processing
-                    q.offer(new int[]{newRow, newCol});
-                }
-            }
+        // Check if the color at the source point is already the new color
+        if (originalColor != color) {
+	        helper(image, sr, sc, color, originalColor);
         }
-
-        return image;        
+        
+        return image;
+    }
+    
+    private void helper(int [][] image, int r, int c, int color, int originalColor) {
+	    // Check if the current element is out of bounds or already changed
+	    if (r < 0 || c < 0 || r >= image.length || c >= image[0].length || image[r][c] != originalColor) {
+		    return;
+	    }
+	    
+	    // Set the current element to the new color
+	    image[r][c] = color;
+	    
+	    // Recursively call floodFillHelper on neighboring elements
+	    for (int[] dir : DIRECTIONS) {
+		    helper(image, r + dir[0], c + dir[1], color, originalColor);
+	    }
     }
 }
