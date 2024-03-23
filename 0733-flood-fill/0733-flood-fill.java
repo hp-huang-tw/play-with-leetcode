a18f93ee4cfdd504cfab59e76baeb333d4eecfda
hp.huang <stitchmos@gmail.com>
2024-03-23 19:53:33 +0800

    Time: 0 ms (100.00%), Space: 44.7 MB (77.91%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,34 +1,40 @@
 class Solution {
-    
-    // O(m*n), O(m*n)
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        // Define the target color `originalColor` at the source point (sr, sc). 
-        // This is the color of the region we want to flood fill.
-        int originalColor = image[sr][sc];
+        int sColor = image[sr][sc];
         
-        // Check if the color at the source point is already the new color
-        if (originalColor != color) {
-            floodFillHelper(image, sr, sc, originalColor, color);
+        if (sColor != color) {
+            helper(image, sr, sc, color, sColor);
         }
-        
         return image;
     }
     
-    
-    private void floodFillHelper(int[][] image, int r, int c, int originalColor, int newColor) {
-        // Check if the current element is out of bounds or already changed
-        if (r < 0 || r >= image.length || c < 0 || c >= image[0].length || image[r][c] != originalColor) 
+    private void helper(int[][] image, int r, int c, int color, int sColor) {
+        if (r < 0 || c < 0 || r >= image.length || c >= image[0].length || image[r][c] != sColor) 
             return;
         
-        // Set the current element to the new color
-        image[r][c] = newColor;
+        image[r][c] = color;
         
-        // Define directions: top, bottom, left, right
-        int[][] directions = {{-1, 0,}, {1, 0}, {0, -1}, {0, 1}};
+        int[][] dirs = new int[][] {
+            {-1, 0}, {1 , 0}, {0, -1}, {0, 1}
+        };
         
-        // Recursively call floodFillHelper on neighboring elements
-        for (int[] dir : directions) {
-            floodFillHelper(image, r + dir[0], c + dir[1], originalColor, newColor);
+        for (int[] dir : dirs) {
+            helper(image, r + dir[0], c + dir[1], color, sColor);
         }
     }
 }
+
+
+// [1,1,1],
+// [1,1,0],
+// [1,0,1]
+
+
+// [2,2,2],
+// [2,2,0],
+// [2,0,1]
+
+
+// [2,2,2],
+// [2,2,0],
+// [2,0,1]
