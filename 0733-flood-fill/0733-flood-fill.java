77003bd7cb8d686e984cf638506bd9a0f91ea398
hp.huang <stitchmos@gmail.com>
2024-03-23 20:54:45 +0800

    Time: 0 ms (100.00%), Space: 45.1 MB (17.55%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,40 +1,26 @@
 class Solution {
+		// top, down, left, right
+		int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+		
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int sColor = image[sr][sc];
+        int originalColor = image[sr][sc];
         
-        if (sColor != color) {
-            helper(image, sr, sc, color, sColor);
+        if (originalColor != color) {
+	        helper(image, sr, sc, color, originalColor);
         }
+        
         return image;
     }
     
-    private void helper(int[][] image, int r, int c, int color, int sColor) {
-        if (r < 0 || c < 0 || r >= image.length || c >= image[0].length || image[r][c] != sColor) 
-            return;
-        
-        image[r][c] = color;
-        
-        int[][] dirs = new int[][] {
-            {-1, 0}, {1 , 0}, {0, -1}, {0, 1}
-        };
-        
-        for (int[] dir : dirs) {
-            helper(image, r + dir[0], c + dir[1], color, sColor);
-        }
+    private void helper(int [][] image, int r, int c, int color, int originalColor) {
+	    if (r < 0 || c < 0 || r >= image.length || c >= image[0].length || image[r][c] != originalColor) {
+		    return;
+	    }
+	    
+	    image[r][c] = color;
+	    
+	    for (int[] dir : directions) {
+		    helper(image, r + dir[0], c + dir[1], color, originalColor);
+	    }
     }
 }
-
-
-// [1,1,1],
-// [1,1,0],
-// [1,0,1]
-
-
-// [2,2,2],
-// [2,2,0],
-// [2,0,1]
-
-
-// [2,2,2],
-// [2,2,0],
-// [2,0,1]
