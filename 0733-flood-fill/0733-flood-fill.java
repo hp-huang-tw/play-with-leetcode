9c18cfeb7a79314332cdbca36444093b9c61cf27
hp.huang <stitchmos@gmail.com>
2024-03-23 21:12:11 +0800

    Time: 1 ms (39.63%), Space: 44.8 MB (63.67%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,32 +1,33 @@
 class Solution {
-		// Define directions: top, bottom, left, right
-		int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-		
+    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        // Define the target color `originalColor` at the source point (sr, sc). 
-		    // This is the color of the region we want to flood fill.
         int originalColor = image[sr][sc];
         
-        // Check if the color at the source point is already the new color
-        if (originalColor != color) {
-	        helper(image, sr, sc, color, originalColor);
+        if (originalColor == color) return image;
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{sr, sc});
+        
+        while (!q.isEmpty()) {
+            int[] pos = q.poll();
+            int r = pos[0];
+            int c = pos[1];
+            
+            if (r < 0 || c < 0 || r >= image.length || c >= image[0].length || image[r][c] != originalColor) {
+                continue;
+            }
+            
+            
+            image[r][c] = color;
+
+            for (int[] dir : directions) {
+                q.offer(new int[]{r + dir[0], c + dir[1]});
+            }
         }
         
+        
         return image;
-    }
-    
-    private void helper(int [][] image, int r, int c, int color, int originalColor) {
-	    // Check if the current element is out of bounds or already changed
-	    if (r < 0 || c < 0 || r >= image.length || c >= image[0].length || image[r][c] != originalColor) {
-		    return;
-	    }
-	    
-	    // Set the current element to the new color
-	    image[r][c] = color;
-	    
-	    // Recursively call floodFillHelper on neighboring elements
-	    for (int[] dir : directions) {
-		    helper(image, r + dir[0], c + dir[1], color, originalColor);
-	    }
+        
     }
 }
