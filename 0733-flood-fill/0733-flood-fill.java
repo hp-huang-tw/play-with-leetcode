ef2de2bbc760d005be47113238847aae55d5ca2b
hp.huang <stitchmos@gmail.com>
2024-03-23 21:21:49 +0800

    Time: 1 ms (39.63%), Space: 44.3 MB (96.74%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,9 +1,10 @@
 class Solution {
-    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int originalColor = image[sr][sc];
         
+        // No need to flood fill if the new color is the same as the original color
         if (originalColor == color) return image;
         
         Queue<int[]> q = new LinkedList<>();
@@ -14,20 +15,22 @@ class Solution {
             int r = currentPosition[0];
             int c = currentPosition[1];
             
-            image[r][c] = color;
+            // Fill the current cell with the new color
+	        image[r][c] = color; 
+            
 
-            for (int[] dir : directions) {
+			// Explore neighbors in all four directions
+            for (int[] dir : DIRECTIONS) {
                 int newRow = r + dir[0];
                 int newCol = c + dir[1];
-                if (newRow >= 0 && newCol >= 0 && newRow < image.length && newCol < image[0].length && image[newRow][newCol] == originalColor) {
-                q.offer(new int[]{newRow, newCol});
-            }
                 
+                if (newRow >= 0 && newCol >= 0 && newRow < image.length && newCol < image[0].length && image[newRow][newCol] == originalColor) {
+                    // Add neighboring cell to the queue for processing
+                    q.offer(new int[]{newRow, newCol});
+                }
             }
         }
-        
-        
-        return image;
-        
+
+        return image;        
     }
 }
