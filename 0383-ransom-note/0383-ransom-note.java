691cb5ea58e175fb759f555ed66e23bcb834d71e
hp.huang <stitchmos@gmail.com>
2024-03-25 00:05:08 +0800

    Time: 1 ms (99.15%), Space: 44.1 MB (97.24%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,19 +1,22 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] magazineWordCount = new int[26];
+		// Create an integer array to store character frequencies
+        int[] charFreq = new int[26];
         
-        for (char c : magazine.toCharArray()) {
-            magazineWordCount[c - 'a']++;
+        // Populate the array with character frequencies from magazine
+        for (char ch : magazine.toCharArray()) {
+            charFreq[ch - 'a']++;
         }
         
-        for (char c : ransomNote.toCharArray()) {
-            int pos = c - 'a';
-            magazineWordCount[pos]--;
+        // Check character frequencies for ransomNote
+        for (char ch : ransomNote.toCharArray()) {
+            int index = ch - 'a';
+            charFreq[index]--;
             
-            if (magazineWordCount[pos] < 0) return false;
+            if (charFreq[index] < 0) return false;
         }
         
-        
+        // If all characters are found and frequencies are valid, return true
         return true;
     }
 }
