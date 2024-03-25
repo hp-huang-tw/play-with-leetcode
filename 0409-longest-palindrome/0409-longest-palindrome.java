5699f095f79b29098f99944f568ed9d9b9a8e4b9
hp.huang <stitchmos@gmail.com>
2024-03-25 16:23:49 +0800

    Time: 7 ms (50.17%), Space: 41 MB (99.47%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> charFreq = new HashMap<>();
+        
+        for (char ch : s.toCharArray()) {
+            charFreq.put(ch, 1 + charFreq.getOrDefault(ch, 0));
+        }
+        
+        int len = 0;
+        boolean foundOdd = false;
+        
+        for (int freq : charFreq.values()) { 
+            // odd = 1, even = 0
+            if (freq % 2 == 0) {
+                len += freq;
+            } else {
+                len += freq - 1;
+                foundOdd = true;   
+            }
+        }
+        
+        if (foundOdd) {
+            len++;
+        }
+        
+        return len;
+    }
+}
+
+
+// multiple odds
+// a b [cccc] [dd]  -> [a|b]
+// 0+0+ 4    + 2  + 1  = 7 
+
+// [aa]a [bb]b [cccc]c d -> [a|b|c|d]
+// 2 + 2 + 4 + 1 = 9 
