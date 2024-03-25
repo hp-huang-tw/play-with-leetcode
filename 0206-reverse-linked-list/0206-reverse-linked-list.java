4cec8c9ad8764b525d30c327438c355a39ca3edb
hp.huang <stitchmos@gmail.com>
2024-03-25 18:37:50 +0800

    Time: 0 ms (100.00%), Space: 42.2 MB (64.32%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,16 +10,16 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        if ( head == null || head.next == null) {
+            return head;
         }
 
-        return prev;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
     }
 }
+
+
