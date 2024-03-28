f490d8ac01f4b885637765adc26fe460917c0ca9
hp.huang <stitchmos@gmail.com>
2024-03-28 20:55:37 +0800

    Time: 30 ms (59.60%), Space: 50.6 MB (79.25%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -2,14 +2,29 @@ class PointsComparator implements Comparator<int[]> {
     
     @Override
     public int compare(int[] a, int[] b) {
-        return (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]);
+        return  (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1]); // descending
     }
     
 } 
     
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, new PointsComparator());
-        return Arrays.copyOfRange(points, 0, k);
+        PriorityQueue<int []> pq = new PriorityQueue<>(new PointsComparator());
+        
+        for (int[] point : points) {
+            pq.offer(point);
+            if (pq.size() > k) {
+                pq.poll(); // remove in front of element (max)
+            }
+        }
+        
+        int[][] res = new int[k][2];
+        
+        int i = k;
+        while (i > 0) {
+            res[--i] = pq.poll();
+        }
+        
+        return res;
     }
 }
