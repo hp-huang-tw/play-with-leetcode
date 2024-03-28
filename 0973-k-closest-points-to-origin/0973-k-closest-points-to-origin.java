daf6524c85b37d0b893f656225ae3e81a8f62727
hp.huang <stitchmos@gmail.com>
2024-03-28 18:15:22 +0800

    Time: 22 ms (83.69%), Space: 55.6 MB (10.94%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,6 +1,15 @@
+class PointsComparator implements Comparator<int[]> {
+    
+    @Override
+    public int compare(int[] a, int[] b) {
+        return (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]);
+    }
+    
+} 
+    
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (a, b) -> (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]));
+        Arrays.sort(points, new PointsComparator());
         return Arrays.copyOfRange(points, 0, k);
     }
 }
